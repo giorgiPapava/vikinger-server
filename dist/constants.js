d39cc96ccc8beb7208a4343df9ed3c79c139b021
gpapava <gpapava@affix.pro>
2021-06-14 00:16:48 +0400

    Started implementing back architecture

---
+++ dist/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const __prod__ = process.env.NODE_ENV === 'production';
+exports.default = {
+    __prod__,
+};
+//# sourceMappingURL=constants.js.map
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// eslint-disable-next-line no-underscore-dangle
+const __prod__ = process.env.NODE_ENV === 'production'
+
+export default {
+  __prod__,
+}
